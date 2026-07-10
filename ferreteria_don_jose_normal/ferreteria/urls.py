99ac2c49c14a3ec8ab0900b8083127ba7a693782
juanp65 <juanpixhd12@gmail.com>
2026-07-10 02:04:14 -0400

    Update urls.py

--- ferreteria_don_jose_normal/ferreteria/urls.py
+++ ferreteria_don_jose_normal/ferreteria/urls.py
@@ -1,12 +1,22 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('tienda.urls')),
-]
+    # Administración de Django
+    path("admin/", admin.site.urls),
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Rutas de la tienda
+    path("", include("tienda.urls")),
+
+    # Imágenes de productos guardadas en la carpeta media
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
